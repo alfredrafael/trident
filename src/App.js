fc4aefacbd1b682b3e23679ae2dc9d875eaa951b
alfredrafael <alfredrafael@gmail.com>
2020-07-28 07:14:32 -0400

    Changes routes for gh-pages from: https://dev.to/caffiendkitten/react-router-with-github-pages-en3

--- src/App.js
+++ src/App.js
@@ -18,9 +18,8 @@ const App = () => {
 
         {/* //////////////////////////////////////////////////////////////////////// */}
         <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/page-2" component={Page2} />
-
+          <Route path="/" exact render={() => <Home/> } />
+          <Route path="/page-2" exact render={() => <Page2/> } />
           <Route render={() => <h2 style={{ paddingTop: '15%' }}>404 Page not found</h2>} />
 
         </Switch>
