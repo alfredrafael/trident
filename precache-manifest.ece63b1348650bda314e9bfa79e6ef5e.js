b640b7ce99515a728247818eff7f10a2e4fea1c6
alfredrafael <alfredrafael@gmail.com>
2020-07-28 07:01:50 -0400

    Updates

---
+++ precache-manifest.ece63b1348650bda314e9bfa79e6ef5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473adcdc8f8253333ecb789dc8867945",
+    "url": "/trident/index.html"
+  },
+  {
+    "revision": "076111857c95863c681e",
+    "url": "/trident/static/css/main.f502d89f.chunk.css"
+  },
+  {
+    "revision": "cbf68e3d002d3e1e7a7d",
+    "url": "/trident/static/js/2.1bb7a622.chunk.js"
+  },
+  {
+    "revision": "5d8326d3efa676d3ffe075800bbd250c",
+    "url": "/trident/static/js/2.1bb7a622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "076111857c95863c681e",
+    "url": "/trident/static/js/main.adc21018.chunk.js"
+  },
+  {
+    "revision": "6cf6f832e64070bef366",
+    "url": "/trident/static/js/runtime-main.46d83435.js"
+  }
+]);
