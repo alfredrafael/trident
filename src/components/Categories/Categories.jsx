5273ae6911fb8dd37b2fe4de2d8b6cc5ad61c661
alfredrafael <alfredrafael@gmail.com>
2020-08-13 14:45:32 -0400

    Removes cardBody from <Category /> cards

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -26,16 +26,17 @@ const MainMenu = () => {
         
         <a href={item.categoryUrl} style={{textDecoration: 'none', color: 'inherit'}}>
         <Card key={i} className="highlight-on-hover" style={{margin: '2% 1%', borderRadius: '15px'}}>
-        <CardHeader style={{background: '#7a1f1f', color: 'white', height: '20%', borderTopLeftRadius: '15px', borderTopRightRadius: '15px'}}>
+        <CardHeader style={{background: '#7a1f1f', color: 'white', height: '20%', borderRadius: '10px'}}>
             <h4 style={{margin: '-1% 0'}}>{item.name}</h4>
         </CardHeader>
-        
-        <CardBody>
-        <CardText style={{textDecoration: 'none', color: 'inherit'}} >{item.description}</CardText>
-        
-        </CardBody>
-        </Card>
-        </a>
+       
+        {/* 
+            <CardBody>
+                <CardText style={{textDecoration: 'none', color: 'inherit'}} >{item.description}</CardText>
+            </CardBody>
+        */}
+            </Card>
+            </a>
         </div> 
     });
 
@@ -44,7 +45,7 @@ const MainMenu = () => {
         <React.Fragment>
         <Fade>
 
-        <div style={{paddingTop: '3%'}} id="categories">
+        <div style={{paddingTop: '3%'}} id="categories" >
 
 
         <h2 style={{
@@ -59,6 +60,10 @@ const MainMenu = () => {
             <div className="row">
             <br/>
                 {renderCategories}
+            </div>
+
+            <div style={{height: '10vh'}}>
+            
             </div>
 
 
